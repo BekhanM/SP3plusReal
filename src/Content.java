111c1a390a5b053eb35ccf7d8a25f5d6d829a358
OttoDyva <cph-ot@cphbusiness.dk>
2023-11-20 11:27:52 +0100

    Adding classes Content & WatchedList

---
+++ src/Content.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Content {
+    ArrayList<Media> media = new ArrayList<>(); // IKKE SIKKER PÅ AT DET ER EN ARRAYLIST!!
+
+    public void addMovie() {
+        return;
+    }
+
+    public void addSeries() {
+        return;
+    }
+
+    public void removeMovie() {
+        return;
+    }
+
+    public void removeSeries()
+    {
+        return;
+    }
+}
---
+++ src/WatchedList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class WatchedList
+{
+    ArrayList<Media> watchedList = new ArrayList<>(); // IKKE SIKKER PÅ DET ER EN ARRAYLIST!!!
+
+    public void addToWatchedList()
+    {
+        return;
+    }
+
+    public void removeFromWatchedList()
+    {
+        return;
+    }
+}
