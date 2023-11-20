aa8f4a1c66612343739e0b3d13ba00e4346cdb31
BekhanM <therealbekhan@gmail.com>
2023-11-20 11:36:33 +0100

    StreamingService class

---
+++ src/StreamingService.java
@@ -0,0 +1,65 @@
+import java.util.List;
+
+public class StreamingService {
+    private FileIO io = new FileIO();
+    private TextUI ui = new TextUI();
+    private List<Content> content;
+    private List<User> users;
+
+    public StreamingService(FileIO io, TextUI ui, List<Content> content, List<User> users) {
+        this.io = io;
+        this.ui = ui;
+        this.content = content;
+        this.users = users;
+    }
+
+    public void setup() {
+
+    }
+
+    public void startMenu() {
+
+    }
+
+    public void mainMenu() {
+
+    }
+
+    public void Login(){
+
+    }
+
+    public void Logout(){
+
+    }
+
+    public void addUser(User user){
+
+    }
+
+    public void removeUser(User user){
+
+    }
+
+    public void searchAll(){
+
+    }
+
+    public void searchByName(){
+
+    }
+
+    public void searchByDateOfRelease(){
+
+    }
+
+    public void searchByRating(){
+
+    }
+
+    public void searchByGenre(){
+
+    }
+
+
+}
