5bfc28efc75cf5e0266d40b772c5c00840124bbf
Ömer Øcalan <cph-oo203@cphbusiness.dk>
2023-11-20 11:33:15 +0100

    DataBaseIO initial update

---
+++ src/DataBaseIO.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DataBaseIO {
+
+    Scanner scanner;
+
+    File file;
+
+    public ArrayList<User> readUserData(String path){
+        return null;
+    }
+
+    public void saveUserData(ArrayList<User> users){
+    }
+
+    public ArrayList<String> readMovieData(String path, int length){
+        return null;
+    }
+
+    public ArrayList<String> readSeriesData(String path, int length){
+        return null;
+    }
+}
