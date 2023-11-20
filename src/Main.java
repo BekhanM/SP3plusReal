b5929862ba685dfb06a9cc38978600fa8ce3503e
BekhanM <therealbekhan@gmail.com>
2023-11-20 14:30:35 +0100

    StreamingService class

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,5 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-FileIO fileData = new FileIO();
-
-
-FileIO fileio = new FileIO(;
-fileio.readMovie("src/100bedstefilm");
-        String password = "wsw";
-
-        DataValidator dataValidator = new DataValidator();
-
-        System.out.println(dataValidator.validatePassword(password));
-
     }
 }
