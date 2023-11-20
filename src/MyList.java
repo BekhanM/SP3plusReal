3da89d036677b81c22c01d67575a5d62a5b74222
OttoDyva <cph-ot@cphbusiness.dk>
2023-11-20 10:58:42 +0100

    Adding myList,TextUI & User

---
+++ src/MyList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class MyList
+{
+    ArrayList<Media> myList = new ArrayList<>();
+
+    public void addToMyList()
+    {
+        return;
+    }
+
+    public void removeFromMyList()
+    {
+        return;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class User
+{
+    private String username;
+    private String password;
+
+    public User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+
+    ArrayList<Media> media = new ArrayList<>();
+}
