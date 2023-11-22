5d84cf75eb43ff51d5b5280418ef583d48c3d49e
BekhanM <therealbekhan@gmail.com>
2023-11-22 11:07:45 +0100

    bingobongo2

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -11,6 +11,18 @@ public class StreamingService {
     private TextUI ui = new TextUI();
     private List<Content> content;
 
+    public void setup() {
+        ArrayList<String> userData = io.readUserData();
+
+        ArrayList<String> movieData = io.readMovieData();
+
+        ArrayList<String> seriesData = io.readSeriesData();
+
+        DataValidator dataValidator = new DataValidator();
+
+        ArrayList<User> users = new ArrayList<>();
+    }
+
     public void startMenu() {
         ui.displayMessage("Hej og velkommen til landets værste streamingtjeneste");
         String i = ui.getInput("Vælg en funktion: \n 1) Logge ind. \n 2) Lave en ny bruger.");
@@ -37,8 +49,7 @@ public class StreamingService {
         }
     }
 
-    public void loginPassword(String user){
-        String userInputUsername = user;
+    public void loginPassword(String user) {
         String userInputPassword = ui.getInput("Kodeord: ");
         if (dataValidator.checkLoginPassword(userData, userInputPassword)) {
             ui.displayMessage("Du er nu logget ind som: " + user);
@@ -46,9 +57,19 @@ public class StreamingService {
             loginPassword(user);
         }
     }
-    public void Logout() {
 
+    public void logout() {
+        String i = ui.getInput("Er du sikker du vil logge ud, bro?\nTast 1 hvis du gerne vil logge ud:\nTast 2 hvis du ikke vil logge ud:");
+        if (i.equals("1")) {
+            startMenu();
+        } else if (i.equals("2")) {
+            mainMenu();
+        } else {
+            ui.displayMessage("Forkert valg. Vælg funktion 1 eller 2.");
+            logout();
+        }
     }
+
 
     public void addUser() {
         String userInputUsername = ui.getInput("Indtast et nyt brugernavn: ");
@@ -71,26 +92,50 @@ public class StreamingService {
         io.saveUserData(users);
     }
 
-    public void removeUser(User user) {
 
+    public void removeUser() {
+        String i = ui.getInput("Er du sikker du vil fjerne din konto ud, bro?\nTast 1 hvis du gerne vil slette din konto:\nTast 2 hvis du ikke vil slette din konto:");
+        if (i.equals("1")) {
+            String uname = ui.getInput("Indtast brugernavn: ");
+            String pword = ui.getInput("Indtast kodeord: ");
+            terminateUser(uname, pword);
+        } else if (i.equals("2")) {
+            mainMenu();
+        }
     }
-    public void displaySeries(){
+
+    public void terminateUser(String username, String password) {
+        User userToRemove = new User(username, password);
+        for (User currentUser : users) {
+            if (currentUser.equals(userToRemove)) {
+            }
+        }
+
+        for (User user : users) {
+            users.remove(user);
+            userData.remove(user.toString());
+        }
+        io.saveUserData(users);
+    }
+
+
+    public void displaySeries() {
         //-----------Printer listen af SERIES i en pen format------------
-        Series series = new Series("","","",0,"","");
+        Series series = new Series("", "", "", 0, "", "");
         List<Series> serie = series.seriesSeparator();
         for (Series s : serie) {
             System.out.println(s);
 
         }
     }
-    public void displayMovies(){
+
+    public void displayMovies() {
         //-----------Printer listen af movies i en pen format------------
-        Movie movie = new Movie("dasd","132","bingchillin",6.2);
+        Movie movie = new Movie("dasd", "132", "bingchillin", 6.2);
         List<Movie> movies = movie.movieSeparator();
         for (Movie m : movies) {
             System.out.println(m);
         }
-
     }
 
     public void searchAll() {
@@ -115,7 +160,6 @@ public class StreamingService {
         ui.displayMessage("Media not found, try again");
         searchAll();
     }
-
 
 
     public void searchByName() {
