d0b0abad08952134dbe51313a472a8b7df795d21
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2023-11-23 10:18:52 +0100

    toString

--- src/Series.java
+++ src/Series.java
@@ -60,29 +60,38 @@ public class Series extends Media {
     @Override
     public String toString()
     {
-        return super.toString() + " Season: " + getSeason() + "Episode: " + getEpisode();
+        return super.toString() + " Season: " + getSeason() + " Episodes: " + getEpisode();
     }
 
 
     public List<Series> seriesSeparator() {
         List<String> data = io.readSeriesData();
         for (String s : data) {
+
             String[] row = s.split(";");
             String title = row[0].trim();
             String releaseDate = row[1].trim();
             String genre = row[2].trim();
             double rating = Double.parseDouble(row[3].replace(",", ".").trim());
 
-            String[] seasonAndEpisodes = row[4].split(","); // splitter sesonger og episoder med comma
+            //Splitteren for seasong og episoder
+            String[] seasonAndEpisodes = row[4].split(",");
+            String totalSeasons = String.valueOf(seasonAndEpisodes.length);
+            int totalEpisodes = 0;
             for (String seasonAndEpisode : seasonAndEpisodes) {
-                String[] parts = seasonAndEpisode.trim().split("-"); // splitter season(ental) og episode
-                String season = parts[0].trim();
-                String episode = parts[1].trim();
-                registerSeries(title, releaseDate, genre, rating, season+" ", episode);
+                String[] parts = seasonAndEpisode.trim().split("-");
+
+
+                int episode = Integer.parseInt(parts[1].trim());
+                totalEpisodes += episode;
+
             }
+
+            registerSeries(title, releaseDate, genre, rating, totalSeasons, String.valueOf(totalEpisodes));
         }
         return seriesList;
     }
+
 
     private void registerSeries(String title,String releaseDate, String genre, double rating, String season, String episode) {
         Series series=new Series(title,releaseDate,genre,rating,season,episode);
